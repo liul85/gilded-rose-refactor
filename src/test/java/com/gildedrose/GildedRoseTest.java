b3912e736908ba717fd193e8688abc7924e8181d
liul85 <liul85@gmail.com>
2019-09-03 07:35:36 +0800

    add App output test first

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,20 +1,20 @@
 package com.gildedrose;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].name, is("foo"));
-        assertThat(app.items[0].quality, is(0));
-        assertThat(app.items[0].sellIn, is(-1));
+    public void shouldGetCorrectAppOutput() throws IOException {
+        String appOutput = TexttestFixture.getAppOutput();
+        assertThat(appOutput, is(new String(Files.readAllBytes(Paths.get("src/test/output.txt")))));
     }
 
 }
