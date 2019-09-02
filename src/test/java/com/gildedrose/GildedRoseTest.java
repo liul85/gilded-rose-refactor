dee8876f1ccd30c52e2021f710be0c40e08662fc
liul85 <liul85@gmail.com>
2019-09-03 07:10:49 +0800

    init repo

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name, is("foo"));
+        assertThat(app.items[0].quality, is(0));
+        assertThat(app.items[0].sellIn, is(-1));
+    }
+
+}
